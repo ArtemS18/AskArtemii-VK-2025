d68cadbe3e5badfb5e5febc7c7bcad2d62aecf34
Artemii <rerttdtdf@gmail.com>
2025-11-13 00:53:29 +0300

    add alembic

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,8 +3,10 @@ from .questions import QuestionORM, QuestionTagsORM
 from .likes import AnswerLikeORM, QuestionLikeORM
 from .users import UserORM, UserProfileORM
 from .tags import TagORM
+from .base import BaseORM
 
 __all__ = (
+    "BaseORM",
     "AnswerORM", 
     "QuestionORM", 
     "QuestionTagsORM", 
